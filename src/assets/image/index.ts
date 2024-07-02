51fd6fade5c5a28864040dbe16098fc66c813907
uv-deva <yuvaraj.d@webllisto.com>
2024-07-02 19:13:50 +0530

    index

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,7 +1,7 @@
 import prisma from "./prisma-logo-dark.png"
 import prismaLogo from "./Prism.png"
 import bgLight from "./bg-light.png"
-import { WalletConnectIcon } from "../Icons"
+import { WalletConnectIcon } from "../icons"
 
 export const IMG = {
     prismLogo : prisma,
